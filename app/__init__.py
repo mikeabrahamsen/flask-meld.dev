1ae0283655bce64e0ddd45100121ce643e7c26a2
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-01-28 22:03:43 -0700

    remove migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
-from flask_migrate import Migrate
 from config import config
 from flask_meld import Meld
 from .db import db, seed_database
 from app import models
 
 # extensions
-migrate = Migrate()
-
 meld = Meld()
 
 
@@ -15,7 +12,6 @@ def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     db.init_app(app)
-    migrate.init_app(app, db)
 
     meld.init_app(app)
 
