91246efacc97513a39b1ce91a5fc5051f66020ee
Michael Abrahamsen <mail@michaelabrahamsen.com>
2021-01-28 22:00:54 -0700

    use in memory database

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-502&5Rz3secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
 class DevelopmentConfig(Config):
